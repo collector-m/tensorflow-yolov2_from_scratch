7d50fc027b4aaaf54b4dd4fbec85556fdf8493d4
YunYang1994 <YunYang1994@github.com>
2019-01-12 18:51:23 +0800

    train

--- train.py
+++ train.py
@@ -17,6 +17,8 @@ from core import backbone, utils
 
 WARM_UP_BATCHES  = 0
 BATCH_SIZE       = 1
+EPOCHS           = 250 * 1000
+LR               = .5e-3
 GRID_H , GRID_W  = 13, 13
 IMAGE_H, IMAGE_W = 416, 416
 ANCHORS          = [0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828]
@@ -48,22 +50,30 @@ output = tf.keras.layers.Conv2D(NUM_ANCHORS * (5 + NUM_CLASSES),
 y_pred = tf.reshape(output, shape=[BATCH_SIZE, GRID_H, GRID_W, NUM_ANCHORS, 5+NUM_CLASSES])
 
 loss_items = utils.compute_loss(y_true, y_pred, true_boxes, GRID_H, GRID_W, BATCH_SIZE, ANCHORS, CLASS_WEIGHTS)
+optimizer = tf.train.MomentumOptimizer(LR, momentum=0.9)
+train_op = optimizer.minimize(loss_items[0])
+
+saver = tf.train.Saver(max_to_keep=2)
+write_op = tf.summary.merge_all()
+writer_train = tf.summary.FileWriter("./data/log")
+
+tf.summary.scalar("loss/loss_xy",      loss_items[1])
+tf.summary.scalar("loss/loss_wh",      loss_items[2])
+tf.summary.scalar("loss/loss_conf",    loss_items[3])
+tf.summary.scalar("loss/loss_class",   loss_items[4])
+tf.summary.scalar("yolov2/total_loss", loss_items[0])
+tf.summary.scalar("yolov2/recall_50",  loss_items[5])
+tf.summary.scalar("yolov2/recall_75",  loss_items[6])
+
+
 sess.run(tf.global_variables_initializer())
-
-# saver = tf.train.Saver(max_to_keep=2)
-# write_op = tf.summary.merge_all()
-# writer_train = tf.summary.FileWriter("./data/log")
-
-# tf.summary.scalar("loss/loss_xy",      loss_items[1])
-# tf.summary.scalar("loss/loss_wh",      loss_items[2])
-# tf.summary.scalar("loss/loss_conf",    loss_items[3])
-# tf.summary.scalar("loss/loss_class",   loss_items[4])
-# tf.summary.scalar("yolov2/total_loss", loss_items[0])
-# tf.summary.scalar("yolov2/recall_50",  loss_items[5])
-# tf.summary.scalar("yolov2/recall_75",  loss_items[6])
-
-
-
+for epoch in range(EPOCHS):
+    _, summary = sess.run([train_op, write_op])
+    writer_train.add_summary(summary, global_step=epoch)
+    writer_train.flush()
+    print("=> epoch %d" %epoch)
+    # if epoch % 2000 == 0: saver.save(sess, save_path="./data/checkpoint/yolov2.ckpt", global_step=epoch)
+    saver.save(sess, save_path="./data/checkpoint/yolov2.ckpt", global_step=epoch)
 
 
 
